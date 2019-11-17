c89dbc9a161afdc1500411e082e3eaf9a59f7a47
John Sandfort <john@jsand.us>
2019-11-17 14:54:16 -0500

    ep1 fixes

--- main.lua
+++ main.lua
@@ -2,11 +2,13 @@ local x = 120
 local dir = 1
 
 function love.update(dt)
-  -- this seems like it could easily get out of sync
-  -- why not make it idempotent?
-  if x >= 400 or x < 120 then
-    dir = dir * -1
-    x = x + 1 * dir
+  -- pauses updating if dt is very high
+  if dt > 0.040 then return end
+
+  if x < 120 then
+    dir = 1
+  elseif x > 400 then
+    dir = -1
   end
   x = x + dir * 200 * dt
 end
