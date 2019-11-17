6114483ad866ea18f4d2b15fe47058c22390ba96
John Sandfort <john@jsand.us>
2019-11-17 14:51:07 -0500

    ep1 commentary

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Recursor's Love2D Tutorials
  
  This repo is just to work through a series of tutorials I found on YouTube for
- the Love2D game engine.
+ the Love2D game engine. Code should be largely verbatim as it appears in the
+ videos, but I'll add comments where I have thoughts and questions.
 
 https://www.youtube.com/playlist?list=PLZVNxI_lsRW2kXnJh2BMb6D82HCAoSTUB
--- main.lua
+++ main.lua
@@ -2,6 +2,8 @@ local x = 120
 local dir = 1
 
 function love.update(dt)
+  -- this seems like it could easily get out of sync
+  -- why not make it idempotent?
   if x >= 400 or x < 120 then
     dir = dir * -1
     x = x + 1 * dir
@@ -10,6 +12,7 @@ function love.update(dt)
 end
 
 function love.draw()
+  -- can't you set the color on individual draw function calls?
   love.graphics.setColor(1, 1, 1)
   love.graphics.print("hello canvas", 10, 100)
   love.graphics.setColor(0.5, 0.25, 1)
