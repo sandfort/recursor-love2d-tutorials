94bd9816d99868e236672f3eac837f10181afaba
John Sandfort <john@jsand.us>
2019-11-23 17:19:37 -0500

    ep3: walk cycle animation

--- main.lua
+++ main.lua
@@ -1,18 +1,31 @@
 local hero_atlas
 local hero_sprite
 
-local angle = 0
+--animation parameters
+local fps = 12
+local anim_timer = 1 / fps
+local frame = 1
+local num_frames = 6
+local xoffset
 
 function love.load()
   love.graphics.setDefaultFilter("nearest", "nearest")
   hero_atlas = love.graphics.newImage("assets/gfx/hero.png")
-  hero_sprite = love.graphics.newQuad(32, 16, 16, 16, hero_atlas:getDimensions())
+  hero_sprite = love.graphics.newQuad(16, 32, 16, 16, hero_atlas:getDimensions())
 end
 
 function love.update(dt)
   if dt > 0.035 then return end -- lag guard thingie again
 
-  angle = angle + 27.5 * dt
+  anim_timer = anim_timer - dt
+  
+  if anim_timer <= 0 then
+    anim_timer = 1 / fps
+    frame = frame + 1
+    if frame > num_frames then frame = 1 end
+    xoffset = 16 * frame
+    hero_sprite:setViewport(xoffset, 32, 16, 16)
+  end
 end
 
 function love.draw()
@@ -20,5 +33,5 @@ function love.draw()
   -- setting these to 8 (half of the sprite's height and width)
   -- means that it will rotate from its center
   -- rather than the upper left corner, as it would by default
-  love.graphics.draw(hero_atlas, hero_sprite, 320, 180, math.rad(angle), 4, 4, 8, 8)
+  love.graphics.draw(hero_atlas, hero_sprite, 320, 180, 0, 10, 10, 8, 8)
 end
