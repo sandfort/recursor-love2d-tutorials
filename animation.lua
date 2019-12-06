3ba714f091f0aee06c2bbe99376659147be8bd39
John Sandfort <john@jsand.us>
2019-12-05 22:19:48 -0500

    ep7: add punch with sound effect

--- animation.lua
+++ animation.lua
@@ -2,20 +2,24 @@ local Class = require("class")
 local Animation = Class:derive("Animation")
 local Vector2 = require("vector2")
 
-function Animation:new(xoff, yoff, w, h, ncols, nframes, fps)
+function Animation:new(xoff, yoff, w, h, nframes, ncols, fps, loop)
   self.fps = fps
-  self.timer = 1 / self.fps
-  self.frame = 1
   self.num_frames = nframes
   self.num_columns = ncols
   self.start_offset = Vector2(xoff, yoff)
   self.offset = Vector2()
   self.size = Vector2(w, h)
+  self.loop = loop == nil or loop -- default to true
+
+  self:reset()
 end
 
 function Animation:reset()
   self.timer = 1 / self.fps
   self.frame = 1
+  self.done = false
+  self.offset.x = self.start_offset.x
+  self.offset.y = self.start_offset.y
 end
 
 function Animation:set(quad)
@@ -23,19 +27,27 @@ function Animation:set(quad)
 end
 
 function Animation:update(dt, quad)
-  if self.num_frames <= 1 then return end
+  if self.num_frames <= 1 then
+    return
+  elseif self.timer > 0 then
+    self.timer = self.timer - dt
   
-  self.timer = self.timer - dt
-  
-  if self.timer <= 0 then
-    self.timer = 1 / self.fps
-    self.frame = self.frame + 1
-    if self.frame > self.num_frames then
-      self.frame = 1
+    if self.timer <= 0 then
+      self.timer = 1 / self.fps
+      self.frame = self.frame + 1
+      if self.frame > self.num_frames then
+        if self.loop then
+          self.frame = 1
+        else
+          self.frame = self.num_frames
+          self.timer = 0
+          self.done = true
+        end
+      end
+      self.offset.x = self.start_offset.x + (self.size.x * ((self.frame - 1) % (self.num_columns)))
+      self.offset.y = self.start_offset.y + (self.size.y * math.floor((self.frame - 1) / self.num_columns))
+      quad:setViewport(self.offset.x, self.offset.y, self.size.x, self.size.y)
     end
-    self.offset.x = self.start_offset.x + (self.size.x * ((self.frame - 1) % (self.num_columns)))
-    self.offset.y = self.start_offset.y + (self.size.y * math.floor((self.frame - 1) / self.num_columns))
-    quad:setViewport(self.offset.x, self.offset.y, self.size.x, self.size.y)
   end
 end
 
--- main.lua
+++ main.lua
@@ -4,21 +4,32 @@ local Sprite = require("sprite")
 local hero_atlas
 
 local spr
+local idle = Animation(16, 16, 16, 16, 4, 4, 6)
 local walk = Animation(16, 32, 16, 16, 6, 6, 12)
 local swim = Animation(16, 64, 16, 16, 6, 6, 12)
+local punch= Animation(16, 80, 16, 16, 3, 3, 8, false)
+local snd
 
 function love.load()
   love.graphics.setDefaultFilter("nearest", "nearest")
   hero_atlas = love.graphics.newImage("assets/gfx/hero.png")
 
-  spr = Sprite(hero_atlas, 16, 16, 100, 100, 10, 10, 45)
+  spr = Sprite(hero_atlas, 16, 16, 100, 100, 10, 10)
+  spr:add_animation("idle", idle)
   spr:add_animation("walk", walk)
   spr:add_animation("swim", swim)
-  spr:animate("swim")
+  spr:add_animation("punch", punch)
+  spr:animate("idle")
+  
+  snd = love.audio.newSource("assets/sfx/hit01.wav", "static")
 end
 
 function love.update(dt)
   if dt > 0.035 then return end
+
+  if spr.current_anim == "punch" and spr:animation_finished() then
+    spr:animate("idle")
+  end
   
   spr:update(dt)
 end
@@ -26,4 +37,12 @@ end
 function love.draw()
   love.graphics.clear(.25, .25, 1)
   spr:draw()
+end
+
+function love.keypressed(key)
+  if key == "space" and spr.current_anim ~= "punch" then
+    love.audio.stop(snd)
+    love.audio.play(snd)
+    spr:animate("punch")
+  end
 end
