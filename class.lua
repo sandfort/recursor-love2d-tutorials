c57450ebe0f1df26e57858493a41f71f976ba522
John Sandfort <john@jsand.us>
2019-12-22 18:35:58 -0500

    ep11: add class checking functions

--- class.lua
+++ class.lua
@@ -5,14 +5,40 @@ Class.__index = Class
 function Class:new() end
 
 -- create a new Class type from our base class
-function Class:derive(type)
+function Class:derive(class_type)
+  assert(class ~= nil, "parameter class_type must not be nil!")
+  assert(type(string) == "table", "parameter class_type must be a string!")
+
   local cls = {}
   cls.__call = Class.__call
-  cls.type = type
+  cls.class_type = class_type
   cls.__index = cls
   cls.super = self -- set superclass
   setmetatable(cls, self)
   return cls
+end
+
+function Class:is(class)
+  assert(class ~= nil, "parameter class must not be nil!")
+  assert(type(class) == "table", "parameter class must be a table!")
+
+  local mt = getmetatable(self)
+  
+  while mt do
+    if mt == class then return true end
+    mt = getmetatable(mt)
+  end
+  return false
+end
+
+function Class:is_type(class_type)
+  assert(class ~= nil, "parameter class_type must not be nil!")
+  assert(type(string) == "table", "parameter class_type must be a string!")
+  
+  local base = self
+  while base do
+    if base.class_type == class_type then return true end
+    base = base.super
 end
 
 function Class:__call(...)
@@ -22,7 +48,7 @@ function Class:__call(...)
 end
 
 function Class:get_type()
-  return self.type
+  return self.class_type
 end
 
 return Class
